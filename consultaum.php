a60906559bd6e0752a168fc21aa92233ba3a5823
Patricia Silva <patriciaananias1983@hotmail.com>
2019-07-20 15:51:42 -0300

    Add files via upload

---
+++ consultaum.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<html>
+
+<head>
+<title>  </title>
+</head>
+<body>
+<?php
+  $a=5423;
+  $b=1184;
+ 
+    if ($a > $b){
+echo"a maior que b";
+    }
+    elseif ($a == $b){
+        echo" b igual a ";
+    }
+
+        else{
+
+            echo" b maior que a";
+        } 
+
+    
+
+
+
+?>
+
+</body>
+
+</html>
