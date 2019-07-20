ed67ecfac67249b02af601d3a02c471e85b28f69
Patricia Silva <patriciaananias1983@hotmail.com>
2019-07-20 15:06:01 -0300

    Add files via upload

---
+++ estrutum.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<html>
+
+<head>
+<title>  </title>
+</head>
+<body>
+<?php
+  
+  $i=0;
+
+ 
+      while ($i <= 10){
+          if ($i % 2 == 0 ){
+              echo $i ." Este numero é par </br>" ;
+
+          }
+
+          else{
+              echo $i . "esse numero é impar </br>";
+          }
+
+     $i = $i+1;
+
+
+      } 
+
+    
+?>
+
+</body>
+
+</html>
