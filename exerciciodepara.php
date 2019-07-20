9bdf8807637ef702f6f20e84737657d44d29dd11
Patricia Silva <patriciaananias1983@hotmail.com>
2019-07-20 15:50:28 -0300

    Add files via upload

---
+++ exerciciodepara.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<html>
+
+<head>
+<title>  </title>
+</head>
+<body>
+<?php
+  
+   for($i=0; $i<50; $i++){
+         
+
+          if($i % 2 ==0 ){
+               echo $i ."par </br>"; }
+            else {
+                echo $i . "impar </br>";
+            }
+   }
+    
+?>
+
+</body>
+
+</html>
