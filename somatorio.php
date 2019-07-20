4382e62504398eab1b71c1e9356f8a10496b16dd
Patricia Silva <patriciaananias1983@hotmail.com>
2019-07-20 15:50:01 -0300

    Add files via upload

---
+++ somatorio.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<html>
+
+<head>
+<title>  </title>
+</head>
+<body>
+<?php
+  $a=5423;
+  $b=1184;
+ 
+  echo "o valor do resutado é:" . ($a + $b);
+
+
+?>
+
+</body>
+
+</html>
