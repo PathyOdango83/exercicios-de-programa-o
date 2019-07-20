7741e5e897d9b1832f167bc8b42c1428fb35b968
Patricia Silva <patriciaananias1983@hotmail.com>
2019-07-20 15:49:28 -0300

    Add files via upload

---
+++ tabuadadecinco.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<html>
+
+<head>
+<title>  </title>
+</head>
+<body>
+<?php
+  
+
+       echo " <u><strong>Tabuada de 5 </strong></u><br>";
+
+   for($i=0; $i<11; $i++){
+          echo "5 x $i =" . 5 * $i . "<br>";
+   }
+    
+
+?>
+
+</body>
+
+</html>
---
+++ valorescincoequinze.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<html>
+
+<head>
+<title>  </title>
+</head>
+<body>
+<?php
+  
+  $inicial;
+  $finall;
+
+       echo " <u><strong> Numeros entre 5 e 15 </strong></u><br>";
+
+   for($i=5; $i<16; $i++){
+          echo " $i";
+
+         
+        }
+?>
+
+</body>
+
+</html>
